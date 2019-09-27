fe6418cc3e831b4a0cfd5a23223f7edf9c9b7a5d
S.Park <spark323@gmail.com>
2019-09-27 22:37:19 +0900

    Apply Prettier

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import SearchScreen from './src/screens/SearchScreen';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import SearchScreen from "./src/screens/SearchScreen";
 
 const navigator = createStackNavigator(
   {
     Search: SearchScreen
-  }, {
-    initialRouteName: 'Search',
+  },
+  {
+    initialRouteName: "Search",
     defaultNavigationOptions: {
-      title: 'Business Search'
+      title: "Business Search"
     }
   }
 );
